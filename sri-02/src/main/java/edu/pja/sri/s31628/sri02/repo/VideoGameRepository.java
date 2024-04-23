d99419aa6053b8d72884623d0d4da67d645c3c02
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2024-04-24 00:04:16 +0200

    Add query in videoGameRepository

--- sri-02/src/main/java/edu/pja/sri/s31628/sri02/repo/VideoGameRepository.java
+++ sri-02/src/main/java/edu/pja/sri/s31628/sri02/repo/VideoGameRepository.java
@@ -1,12 +1,18 @@
 package edu.pja.sri.s31628.sri02.repo;
 
 import edu.pja.sri.s31628.sri02.model.VideoGame;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VideoGameRepository extends CrudRepository<VideoGame, Long> {
     List<VideoGame> findAll();
+
+    @Query("from VideoGame as game left join fetch game.developers where game.id=:videoGameId")
+    Optional<VideoGame> getVideoGameDetailsById(@Param("videoGameId") Long videoGameId);
 }
--- sri-02/src/main/java/edu/pja/sri/s31628/sri02/rest/DeveloperController.java
+++ sri-02/src/main/java/edu/pja/sri/s31628/sri02/rest/DeveloperController.java
@@ -1,4 +1,9 @@
 package edu.pja.sri.s31628.sri02.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/developers")
 public class DeveloperController {
 }
