2d55e88f2d83407f2262a298e700cfed748cf2ce
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2024-04-24 23:50:06 +0200

    Update DeveloperDtoMapper.java

--- sri-02/src/main/java/edu/pja/sri/s31628/sri02/dto/DeveloperDtoMapper.java
+++ sri-02/src/main/java/edu/pja/sri/s31628/sri02/dto/DeveloperDtoMapper.java
@@ -1,9 +1,23 @@
 package edu.pja.sri.s31628.sri02.dto;
 
+import edu.pja.sri.s31628.sri02.model.Developer;
+import edu.pja.sri.s31628.sri02.model.VideoGame;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DeveloperDtoMapper {
     private ModelMapper modelMapper;
+
+    public DeveloperDetailsDto convertToDetailsDto(Developer dev) {
+        return modelMapper.map(dev, DeveloperDetailsDto.class);
+    }
+
+    public DeveloperDto convertToDto(Developer dev) {
+        return modelMapper.map(dev, DeveloperDto.class);
+    }
+
+    private Developer convertToEntity(DeveloperDto dto) {
+        return modelMapper.map(dto, Developer.class);
+    }
 }
