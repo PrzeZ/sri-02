2c96d266f382b52bc3d6c3f3a46496f46a51e4bd
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2024-04-24 23:12:35 +0200

    Create DeveloperDtoMapper.java

---
+++ sri-02/src/main/java/edu/pja/sri/s31628/sri02/dto/DeveloperDtoMapper.java
@@ -0,0 +1,9 @@
+package edu.pja.sri.s31628.sri02.dto;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeveloperDtoMapper {
+    private ModelMapper modelMapper;
+}
