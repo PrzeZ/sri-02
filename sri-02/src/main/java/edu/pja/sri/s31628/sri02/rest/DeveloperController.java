f2cead35fc26adc28452f9e514e82bd67b348e1d
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2024-04-25 00:03:26 +0200

    Update DeveloperController.java

--- sri-02/src/main/java/edu/pja/sri/s31628/sri02/rest/DeveloperController.java
+++ sri-02/src/main/java/edu/pja/sri/s31628/sri02/rest/DeveloperController.java
@@ -1,9 +1,21 @@
 package edu.pja.sri.s31628.sri02.rest;
 
+import edu.pja.sri.s31628.sri02.dto.DeveloperDtoMapper;
+import edu.pja.sri.s31628.sri02.model.Developer;
+import edu.pja.sri.s31628.sri02.repo.DeveloperRepository;
+import edu.pja.sri.s31628.sri02.repo.VideoGameRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/developers")
+@RequiredArgsConstructor
 public class DeveloperController {
+
+    private DeveloperDtoMapper developerDtoMaper;
+    private VideoGameRepository videoGameRepository;
+    private DeveloperRepository DeveloperRepository;
+    private ModelMapper modelMapper;
 }
