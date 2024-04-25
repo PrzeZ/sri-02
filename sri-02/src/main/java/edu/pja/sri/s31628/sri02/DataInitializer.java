6e431654ae6a89d85cf39dd9fc764f0a54429225
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2024-04-26 01:47:06 +0200

    Update DataInitializer.java

--- sri-02/src/main/java/edu/pja/sri/s31628/sri02/DataInitializer.java
+++ sri-02/src/main/java/edu/pja/sri/s31628/sri02/DataInitializer.java
@@ -1,5 +1,26 @@
 package edu.pja.sri.s31628.sri02;
 
-public class DataInitializer {
+import edu.pja.sri.s31628.sri02.model.VideoGame;
+import edu.pja.sri.s31628.sri02.repo.DeveloperRepository;
+import edu.pja.sri.s31628.sri02.repo.VideoGameRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
+public class DataInitializer implements ApplicationRunner {
+
+    private DeveloperRepository developerRepository;
+    private VideoGameRepository videoGameRepository;
+    public DataInitializer(DeveloperRepository developerRepository, VideoGameRepository videoGameRepository) {
+        this.developerRepository = developerRepository;
+        this.videoGameRepository = videoGameRepository;
+    }
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        VideoGame g1 = VideoGame.builder()
+                .title("Undertale")
+                .releaseYear(2015)
+                .build();
+    }
 }
