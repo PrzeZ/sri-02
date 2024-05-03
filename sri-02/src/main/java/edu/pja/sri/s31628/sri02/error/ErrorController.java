9f97625f3d116d5365c73d20098d05bf2183fa43
PrzeZ <56115183+PrzeZ@users.noreply.github.com>
2024-05-03 03:45:26 +0200

    Add error controller

---
+++ sri-02/src/main/java/edu/pja/sri/s31628/sri02/error/ErrorController.java
@@ -0,0 +1,21 @@
+package edu.pja.sri.s31628.sri02.error;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+
+@RestControllerAdvice
+public class ErrorController {
+    @ExceptionHandler(value = { IllegalArgumentException.class, MethodArgumentNotValidException.class })
+    public ResponseEntity<ErrorMessage> handleBadRequestException(Exception ex) {
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .error("Bad request")
+                .build();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
